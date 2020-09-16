6d55fc32964dfa4a534cc262b81308bd9bd346ab
pelevin-d-y <natpuot1992@gmail.com>
2020-09-16 21:42:17 +0300

    feat: update version

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-install: npm install
-dev: parcel index.pug
+install: npm install  
+dev: parcel index.pug  
 build: parcel build index.pug parcel --public-url ./
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,11 @@
-var jquery = require("jquery");
+import module from './module'
+import jquery from 'jquery'
 window.$ = window.jQuery = jquery;
+
 
 $(document).ready(() => {
   console.log($('body'))
+  console.log('a')
+  module()
 })
+
